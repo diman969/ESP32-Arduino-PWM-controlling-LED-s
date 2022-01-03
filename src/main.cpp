af0dec4ea275101d7164c17f728ecdb1ab712001
diman969 <diman969@gmail.com>
2022-01-03 15:04:00 +0200

    added code for controling 2 leds with separate pwm's

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,35 @@
 #include <Arduino.h>
+#define firstLedPin 16
+#define secondLedPin 17
+#define firstLedChannel 0
+#define secondLedChannel  2
+#define frequency 5000
+#define resolution 8
 
 void setup() {
   // put your setup code here, to run once:
+  ledcSetup(firstLedChannel, frequency, resolution);
+  ledcSetup(secondLedChannel, frequency, resolution);
+
+  ledcAttachPin(firstLedPin, firstLedChannel);
+  ledcAttachPin(secondLedPin, secondLedChannel);
+  
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  for(int dutyCycle = 0, secondDutyCycle = 255; dutyCycle <= 255; dutyCycle++, secondDutyCycle--){   
+    // changing the LED brightness with PWM
+    ledcWrite(firstLedChannel, dutyCycle);
+    ledcWrite(secondLedChannel, secondDutyCycle);
+    delay(15);
+  }
+
+  // decrease the LED brightness
+  for(int dutyCycle = 255, secondDutyCycle = 0; dutyCycle >= 0; dutyCycle--, secondDutyCycle++){
+    // changing the LED brightness with PWM
+    ledcWrite(firstLedChannel, dutyCycle);   
+    ledcWrite(secondLedChannel, secondDutyCycle);
+    delay(15);
+  }
 }
